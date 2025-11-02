ff315cab943cada91db427a1dad6c360c65b858b
viorelpoenariu28 <viorelpoenariu28@gmail.com>
2025-11-02 20:11:17 +0200

    Stilizare-initiala-pag-activitate

--- src/pages/TeacherDashboard/TeacherDashboard.jsx
+++ src/pages/TeacherDashboard/TeacherDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
-import "./TeacherDashboard.css";
+import "./TeacherDashboard";
 
 export default function TeacherDashboard() {
   const [description, setDescription] = useState("");
